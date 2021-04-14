5fcf6ac486b6df4be6d032278c8926b44f627034
AntonM-248 <53975792+AntonM-248@users.noreply.github.com>
2021-04-14 00:04:03 -0400

    Added tests

--- PbDb.Testing/UnitTest1.cs
+++ PbDb.Testing/UnitTest1.cs
@@ -34,6 +34,67 @@ namespace PbDb.Testing
             Assert.True(actual == 42);
         }
 
+        [Fact]
+        public void Test4()
+        {
+            var sut = new RevenueRecord();
+            sut.Week = 42;
+            var actual = sut.Week;
+            Assert.True(actual == 42);
+        }
 
+        [Fact]
+        public void Test5()
+        {
+            var sut = new RevenueRecord();
+            sut.Month = 42;
+            var actual = sut.Month;
+            Assert.True(actual == 42);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            var sut = new RevenueRecord();
+            sut.Revenue = 42;
+            var actual = sut.Revenue;
+            Assert.True(actual == 42);
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            var sut = new RevenueRecord();
+            sut.Revenue = 275;
+            var actual = sut.Revenue;
+            Assert.True(actual == 275);
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            var sut = new RevenueRecord();
+            sut.Week = 300;
+            var actual = sut.Week;
+            Assert.True(actual == 300);
+        }
+
+        [Fact]
+        public void Test9()
+        {
+            var sut = new RevenueRecord();
+            sut.Revenue = (decimal)4000.1;
+            var actual = sut.Revenue;
+            Assert.True(actual == (decimal)4000.1);
+        }
+
+        [Fact]
+        public void Test10()
+        {
+            var sut = new RevenueRecord();
+            sut.Revenue = (decimal)42.765;
+            var actual = sut.Revenue;
+            Assert.True(actual == (decimal)42.765);
+        }
     }
 }
